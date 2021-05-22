1e51127f6ba9bc85333fa1d20c1bfdb8667f7738
marcelbritto <marcelbrittorj@gmail.com>
2021-05-22 17:10:14 -0300

    Inclusao dos Repositorios e do Flyway

---
+++ src/main/java/br/com/marcbritto/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package br.com.marcbritto.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/br/com/marcbritto/api/repositories/LancamentoRepository.java
@@ -0,0 +1,37 @@
+package br.com.marcbritto.api.repositories;
+
+import java.util.List;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.marcbritto.api.entities.Lancamento;
+
+@Transactional(readOnly = true)
+@NamedQueries({
+		@NamedQuery(name = "LancamentoRepository.findByFuncionarioId", 
+				query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+	
+	/**
+	 * Busca de funcionário por Id.
+	 * 
+	 * @param funcionarioId
+	 * @return
+	 */
+	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+
+	/**
+	 * Busca de funcionário por Id, com paginação.
+	 * @param funcionarioId
+	 * @param pageable
+	 * @return
+	 */
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
+}
